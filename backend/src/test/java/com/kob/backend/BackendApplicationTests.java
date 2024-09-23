c2d032227a49254410020ee2f3420217376ea2f6
laosijifeikong <1991740601@qq.com>
2024-09-23 21:18:38 +0800

    创建后端

---
+++ backend/src/test/java/com/kob/backend/BackendApplicationTests.java
@@ -0,0 +1,15 @@
+package com.kob.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendApplicationTests
+{
+
+    @Test
+    void contextLoads()
+    {
+    }
+
+}
