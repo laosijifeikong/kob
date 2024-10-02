da86253402dc15c620d62e328d4c4842544f53bb
laosijifeikong <1991740601@qq.com>
2024-10-02 22:21:51 +0800

    配置Mysql与注册登录模块（中）

--- web/src/App.vue
+++ web/src/App.vue
@@ -1,6 +1,6 @@
 <template>
-  <NavBar/>
-  <router-view/>
+  <NavBar />
+  <router-view />
 </template>
 
 <script>
@@ -8,15 +8,46 @@ import NavBar from './components/NavBar.vue'
 import "bootstrap/dist/css/bootstrap.min.css"
 import "bootstrap/dist/js/bootstrap"
 
-export default{
-  components:{
+export default {
+  components: {
     NavBar
   },
+  setup() {
+    // $.ajax({
+    //   url: "http://localhost:8088/user/account/info",
+    //   type: "get",
+    //   headers: {
+    //     Authorization: "Bearer " + "eyJhbGciOiJIUzI1NiJ9.eyJqdGkiOiJlMTI2YjM5MjVmNDM0YTZkYmFkZmMyMTk3MWJiYWJmMSIsInN1YiI6IjEiLCJpc3MiOiJzZyIsImlhdCI6MTcyNzg1MjMxOCwiZXhwIjoxNzI5MDYxOTE4fQ.nRodz1vbfgS1YDphzpaxmXRkudC367OmJzZhUzGPi8E"
+    //   },
+    //   success(resp) {
+    //     console.log(resp);
+    //   },
+    //   error(resp) {
+    //     console.log(resp);
+    //   }
+    // })
+
+    // $.ajax({
+    //   url: "http://localhost:8088/user/account/register",
+    //   type: "post",
+    //   data: {
+    //     username: "zhangsan",
+    //     password: "123",
+    //     confirmedPassword: "123",
+    //   },
+    //   success(resp) {
+    //     console.log(resp);
+    //   },
+    //   error(resp) {
+    //     console.log(resp);
+    //   }
+    // })
+  }
 }
 </script>
 
 <style>
-body{
+body {
   background-image: url(@/assets/images/background.png);
   background-size: cover;
 }
---
+++ web/src/views/user/account/UserAccountRegisterView.vue
@@ -0,0 +1,17 @@
+<template lang="">
+    <ContentField>注册</ContentField>
+</template>
+
+<script>
+import ContentField from "@/components/ContentField.vue"
+
+export default{
+    components:{
+        ContentField
+    }
+}
+</script>
+
+<style lang="">
+    
+</style>
